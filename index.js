95e23d1f5848dbc230f00e52e8b26db538f313fa
MBMIUN1\user <merpatibiru.78@gmail.com>
2021-07-04 15:29:28 +0700

    test again

--- index.js
+++ index.js
@@ -1,15 +1,23 @@
-const morgan = require("morgan");
 const express = require("express");
+// const multer = require("multer");
+// const upload = multer({dest:'upload/'});
 const app= express();
+const morgan = require("morgan");
 const {
     getConnection,
     executeQuery
 } = require("./utils");
 
-app.use(morgan(':method :url :status :res[content-length] - :response-time ms'));
+app.use(morgan("dev"));
+app.use(express.urlencoded({
+    extended: true
+}));
 
+//users
+app.get("/api/users", function (req,res){
 
-
+    return res.status(200).send("hello");
+});
 app.listen(3000, function () {
     console.log('listening on port 3000');
 })
