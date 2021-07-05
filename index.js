4d0ad7833728c94e55c2e106a0390b18d25a8eb4
MBMIUN1\user <merpatibiru.78@gmail.com>
2021-07-05 15:09:50 +0700

    insert Team

--- index.js
+++ index.js
@@ -1,9 +1,29 @@
+// import
 const express = require("express");
 const morgan = require("morgan");
 const fs = require('fs');
 const  path = require('path');
-const date = require('date-and-time');
 const dateFormat = require("dateformat");
+const jwt = require('jsonwebtoken');
+const multer= require('multer');
+// Create multer object
+let id_team;
+const upload = multer({
+    storage: multer.diskStorage(
+        {
+            destination: function (req, file, cb) {
+                cb(null, 'public/uploads');
+            },
+            filename: function (req, file, cb) {
+                cb(
+                    null,
+                    file.originalname
+                );
+            }
+        }
+    ),
+});
+// morgan format
 let pesan;
 morgan.token('date', function (res) {
     let now = dateFormat(new Date(), "dd/MM/yyyy HH:MM");
@@ -14,14 +34,16 @@ morgan.token('message', function (req,res) {
     return psn;
 });
 morgan.format("formatku","Method:"+':method; '+"URL:"+':url; '+"Status:"+':status; '
-    +"MESSAGE:"+':message; '+"DateTime:"+':date; ' )
+    +"MESSAGE:"+':message; '+"DateTime:"+':date; ' );
+
 const app= express();
 const {
     getConnection,
     executeQuery
 } = require("./utils");
-const accessLogStream = fs.createWriteStream(path.join('./log', '0304.log'), { flags: 'a' });
 
+// logStream
+const accessLogStream = fs.createWriteStream(path.join('./log', '0304.log'), { flags: 'a' });
 app.use(morgan("formatku",
     { stream: accessLogStream }));
 
@@ -29,7 +51,7 @@ app.use(express.urlencoded({
     extended: true
 }));
 
-//users
+// users
 app.post("/api/users", async function (req,res){
     let username;
     if(validateUserName(req.body.username)){
@@ -40,31 +62,117 @@ app.post("/api/users", async function (req,res){
             username = req.body.username;
         }else{
             pesan ="username sudah pernah di gunakan";
-            return res.status(400).send(pesan);
+            return res.status(404).send(pesan);
         }
     }else{
         pesan ="username harus kombinasi huruf dan angka";
-        return res.status(400).send(pesan);
+        return res.status(404).send(pesan);
     }
     let pass= req.body.password;
     let role;
     if(req.body.role_user == "A" || req.body.role_user == "a" ){
-        role = 1;
+        role = 'Admin';
     }else if(req.body.role_user == "U" || req.body.role_user == "u"){
-        role = 2;
+        role = 'Users';
     }else{
         pesan ="Role A untuk admin dan U untuk user";
-        return res.status(400).send(pesan);
+        return res.status(404).send(pesan);
     }
     let query= "INSERT INTO `users`(`id_user`, `username_user`, `password`, `role_user`) VALUES ('','"+username+"','"+pass+"'" +
         ",'"+role+"')";
     let hasil = await executeQuery(query);
     if (hasil.err){
+        pesan=hasil.err;
         return res.status(404).send(hasil.err);
     }else{
-        pesan="berhasil menambahkan user baru";
+        let query ="Select `username_user`, `role_user` from `users` where `username_user` like '%"+username+"%' and `password` = '"+pass+"'";
+        let ambil = await  executeQuery(query);
+        if(ambil.length>=1){
+            pesan="berhasil menambahkan user baru";
+            return res.status(200).send(ambil);
+        }else{
+            pesan="tidak berhasil menambahkan user baru";
+            return res.status(500).send(pesan);
+        }
 
-        return res.status(200).send(pesan);
+    }
+
+});
+app.post("/api/users/login", async function (req,res){
+    let user = req.body.username;
+    let pass =req.body.password;
+    let query = "select users.username_user as 'user',users.id_user as 'id',users.role_user as 'role' from users where username_user like '"+user+"' and password = '"+pass+"'";
+    let hasil = await executeQuery(query);
+    if(hasil.length>=1){
+        const token = jwt.sign({ id: hasil[0]["id"], username: hasil[0]["user"], role: hasil[0]["role"] }
+            , 'private_key');
+        let query ="INSERT INTO `token`(`id_token`, `username`, `jwt_key`) VALUES ('','"+hasil[0]["user"]+"'" +
+            ",'"+token+"')";
+        let insertToken = await executeQuery(query);
+        if(insertToken.err){
+            pesan=insertToken.err;
+            return res.status(404).send(insertToken.err);
+        }else{
+            let query = "SELECT username,jwt_key FROM `token` WHERE jwt_key='"+token+"' and username='"+hasil[0]["user"]+"'";
+            let ambilToken= await executeQuery(query);
+            if (ambilToken.length>0){
+                pesan="Berhasil melakukan login";
+                return res.status(200).send(ambilToken);
+            }else{
+                pesan=ambilToken.err;
+                return res.status(200).send(ambilToken.err);
+            }
+        }
+    }else{
+        pesan=hasil.err;
+        return res.status(404).send("pastikan username dan password benar");
+    }
+});
+// teams
+app.post('/api/teams', upload.single('logo_team'), async function (req, res,next){
+    let query="SELECT users.role_user FROM `token`,`users` where users.username_user = token.username " +
+        "and token.jwt_key ='"+req.body.authorization+"'";
+    let cek= await  executeQuery(query);
+    if(cek.length>0 && cek[0]['role_user']=='Admin'){
+        let nowYear = dateFormat(new Date(), "yyyy");
+        if(req.body.tahun_ditemukan_team>nowYear){
+            pesan="Tahun tidak sesuai"
+            return res.status(404).send(pesan);
+        }
+        let id_team = req.body.nama_team.substr(0,2).toUpperCase();
+        let Newpath;
+        let checkId= await executeQuery("select id_team from teams where id_team like '"+id_team+"%' " +
+            "order by id_team desc");
+        if(checkId.length>0){
+            let lastId= checkId[0]["id_team"].substr(3,3)*1+1;
+            lastId = ('0000'+lastId).slice(-3);
+            id_team = id_team+lastId;
+            let Oldpath =req.file.path;
+            Newpath = 'public/uploads/' + id_team + '.jpg';
+            fs.rename(Oldpath, Newpath, function(err) {
+                if ( err ) console.log('ERROR: ' + err);
+            });
+        }else{
+            id_team=id_team +"001";
+            let Oldpath =req.file.path;
+            Newpath = 'public/uploads/' + id_team + '.jpg';
+            fs.rename(Oldpath, Newpath, function(err) {
+                if ( err ) console.log('ERROR: ' + err);
+            });
+        }
+        let insertTeam = await executeQuery("INSERT INTO `teams`(`id_team`, `nama_team`, `nama_stadio_team`, `tahun_ditemukan_team`, `logo_team`) " +
+            "VALUES ('"+id_team+"','"+req.body.nama_team+"'," +
+            "'"+req.body.nama_stadion+"','"+req.body.tahun_ditemukan_team+"','"+Newpath+"')");
+        if(insertTeam.err){
+            return res.status(500).send(insertTeam.err);
+        }else{
+            pesan="Berhasil menambahkan team";
+            let selectTeam=await executeQuery("SELECT * FROM `teams` WHERE id_team='"+id_team+"'");
+            return res.status(200).send(selectTeam);
+        }
+    }else{
+        pesan="Unauthorized";
+        return res.status(500).send(pesan);
     }
 
 });
